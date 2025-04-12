e14e64a70f8a0dd21526fb228da4171a7eb6d769
Ashraf2711 <ashrafalir2711@gmail.com>
2025-04-12 11:34:48 +0530

    Created Landing Page task using html and tailwindcss

---
+++ Tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+module.exports = {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+      extend: {
+        fontFamily: {
+          rowdies: ['"Rowdies"', 'cursive'],
+          righteous: ['"Righteous"', 'cursive'],
+          saira: ['"Saira"', 'sans-serif'],
+        },
+      },
+    },
+    plugins: [],
+  };
+  
